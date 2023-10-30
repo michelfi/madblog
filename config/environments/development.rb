109411b0b3251c22bf0b916f51913f2ff4ec2faa
michelfi <michel.figueres@gmail.com>
2023-10-30 20:56:11 +0100

    oui

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   port: 587,
   domain: 'gmail.com',
   user_name: 'michel.figueres@gmail.com',
-  password: 'neverforget69007',
+  password: '*****',
   authentication: 'plain',
   enable_starttls_auto: true
 }
